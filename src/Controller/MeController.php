75a9d4287bbbd58ecd57f10f25e597ac35e041ba
caillault jeremy <jc.caillault@outlook.fr>
2026-03-13 14:57:06 +0100

    infos user return en JSON ds la rep http

---
+++ src/Controller/MeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use App\Entity\User;
+
+class MeController extends AbstractController
+{
+    #[Route('/api/me', name: 'api_me', methods: ['GET'])]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function me(): JsonResponse
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        return $this->json([
+            'id'        => $user->getId(),
+            'email'     => $user->getEmail(),
+            'firstName' => $user->getFirstName(),
+            'lastName'  => $user->getLastName(),
+            'roles'     => $user->getRoles(),
+        ]);
+    }
+}
